ea6ba618ad0242ba14d7a2685b0fbcb774906021
lucafisc <lucafischer@fastmail.com>
2022-09-16 20:10:34 +0200

    Add level toggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import "./styles/App.css";
-import "./styles/Animations.css";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { levelData } from "./components/levelData";
+import "./styles/App.css";
+import "./styles/Animations.css";
 function App() {
+  console.log(levelData[0]);
+  const [level, setLevel] = useState(0);
+  useEffect(() => {
+    const backgroundColor = levelData[level]["backgroundColor"];
+    const hoverColor = levelData[level]["hoverColor"];
+    const speed = levelData[level]["speed"];
+    changeStyles(backgroundColor, hoverColor, speed);
+  }, [level]);
+
+  function handleClick() {
+    setLevel((prevLevel) => prevLevel + 1);
+  }
   return (
     <div className="App">
       <Header />
-      <Footer level={3} />
+      <button onClick={handleClick}>+</button>
+      <Footer level={level + 1} />
     </div>
   );
+}
+
+function changeStyles(background, hover, speed) {
+  document.documentElement.style.setProperty("--background", background);
+  document.documentElement.style.setProperty("--hover", hover);
+  document.documentElement.style.setProperty("--speed", speed);
 }
 
 export default App;
