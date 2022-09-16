feec6f9b267241f87a4c353c9e820eb2a105b6ec
lucafisc <lucafischer@fastmail.com>
2022-09-16 16:59:52 +0200

    Update Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Score from "./Score";
 export function Header() {
   return (
     <header className="header">
-      <h1 className="header">memory game</h1>
+      <h1>memory game</h1>
       <Score score={0} />
     </header>
   );
