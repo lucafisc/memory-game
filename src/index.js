d9903a9a6b2ff50b04cfa0777df787e0ede598d8
lucafisc <lucafischer@fastmail.com>
2022-09-18 11:14:39 +0200

    Fix height on mobile

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Div100vh from "react-div-100vh";
+
 import "./styles/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Div100vh>
+      <App />
+    </Div100vh>
   </React.StrictMode>
 );
